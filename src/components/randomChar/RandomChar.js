5c09666e0ced213cbee00232307629e33132dcc4
Alexei <Lie.oo@mail.ru>
2022-09-12 22:35:40 +0300

    services created and RandomChar creation started

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,42 +1,59 @@
 import './randomChar.scss';
-import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
+import MarvelService from '../../services/MarvelService';
+import { Component } from 'react/cjs/react.production.min';
+class RandomChar extends Component {
+    state = {
+        char: {}
+    }
+    marvelService = new MarvelService()
+    componentDidMount(){
+        this.updateChar()
+    }
 
-const RandomChar = () => {
-    return (
-        <div className="randomchar">
-            <div className="randomchar__block">
-                <img src={thor} alt="Random character" className="randomchar__img"/>
-                <div className="randomchar__info">
-                    <p className="randomchar__name">Thor</p>
-                    <p className="randomchar__descr">
-                        As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate...
-                    </p>
-                    <div className="randomchar__btns">
-                        <a href="#" className="button button__main">
-                            <div className="inner">homepage</div>
-                        </a>
-                        <a href="#" className="button button__secondary">
-                            <div className="inner">Wiki</div>
-                        </a>
+    updateChar = () =>{
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000)
+        this.marvelService.getCharacter(id).then(char => this.setState({char}))
+    }
+    render(){
+        const {char:{name,decoration,thumbnail,homepage,wiki}} = this.state
+        const noneImg = thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+        const styleImg = !noneImg ? 'cover' : 'contain'
+        return (
+            <div className="randomchar">
+                <div className="randomchar__block">
+                    <img style={{objectFit: styleImg}} src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                        {decoration}
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href="#" className="button button__main">
+                                <div className="inner">homepage</div>
+                            </a>
+                            <a href="#" className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
                     </div>
                 </div>
+                <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button onClick={this.updateChar} className="button button__main">
+                        <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
             </div>
-            <div className="randomchar__static">
-                <p className="randomchar__title">
-                    Random character for today!<br/>
-                    Do you want to get to know him better?
-                </p>
-                <p className="randomchar__title">
-                    Or choose another one
-                </p>
-                <button className="button button__main">
-                    <div className="inner">try it</div>
-                </button>
-                <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default RandomChar;
---
+++ src/services/MarvelService.js
@@ -0,0 +1,33 @@
+class MarvelService {
+    _baseUrl = 'https://gateway.marvel.com:443/v1/public/'
+    _apiKey = process.env.REACT_APP_MARVEL_API_KEY // api key here
+    
+    getResurce = async(url) =>{
+        const res = await fetch(url)
+
+        if(!res.ok){
+           throw new Error(`По запросу на ${url} получина ожибка ${res.status}`)
+        }
+        return await res.json()
+    }
+    getCharacter = async(id) =>{
+        const char = await this.getResurce(`${this._baseUrl}characters/${id}?${this._apiKey}`)
+        return this._transformChar(char.data.results[0])
+    }
+    getAllCharacters = async() => {
+        const chars = await this.getResurce(`${this._baseUrl}characters?limit=9&${this._apiKey}`)
+        return chars.data.results.map(this._transformChar)
+    }
+
+    _transformChar = (char) =>{
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
+}
+
+export default MarvelService;
